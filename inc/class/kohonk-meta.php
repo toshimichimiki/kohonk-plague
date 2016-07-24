f1f91e99b5a9946b7e0fbaca0c51d75c5c94bfaf
imot <chagotai.han.io@gmail.com>
2016-07-24 15:35:17 +0900

    Support metabox

--- inc/class/kohonk-meta.php
+++ inc/class/kohonk-meta.php
@@ -14,18 +14,42 @@ class KohonkMeta{
 			'context' => 'side',
 			'fields' => array(
 				array(
-					'name' => __('keywords', 'kohonk-plague'),
+					'name' => __('Keywords', 'kohonk-plague'),
 					'id' => $prefix . 'keywords',
 					'type' => 'text',
 				),
 				array(
-					'name' => __('alignment', 'kohonk-plague'),
-					'id' => $prefix . 'align',
+					'name' => __('Writing Mode', 'kohonk-plague'),
+					'id' => $prefix . 'writing_mode',
 					'type' => 'radio',
 					'inline' => false,
+					'std' => 'horizontal-tb',
+					'options' => array(
+						'horizontal-tb' => __('Content flows horizontally from left to right, vertically from top to bottom', 'kohonk-plague'),
+						'vertical-rl' => __('Content flows vertically from top to bottom, horizontally from right to left', 'kohonk-plague'),
+						'vertical-lr' => __('Content flows vertically from top to bottom, horizontally from left to right', 'kohonk-plague'),
+					),
+				),
+				array(
+					'name' => __('Direction', 'kohonk-plague'),
+					'id' => $prefix . 'direction',
+					'type' => 'radio',
+					'inline' => false,
+					'std' => 'ltr',
 					'options' => array(
 						'ltr' => __('Left to right', 'kohonk-plague'),
 						'rtl' => __('Right to left', 'kohonk-plague'),
+					),
+				),
+				array(
+					'name' => __('Alignment', 'kohonk-plague'),
+					'id' => $prefix . 'align',
+					'type' => 'radio',
+					'inline' => false,
+					'std' => 'left',
+					'options' => array(
+						'left' => __('Left', 'kohonk-plague'),
+						'right' => __('Right', 'kohonk-plague'),
 						'center' => __('Centering', 'kohonk-plague'),
 						'justify' => __('Justification', 'kohonk-plague'),
 					),
